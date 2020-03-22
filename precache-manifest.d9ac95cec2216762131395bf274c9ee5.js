85efe6552fc9b6fbfbc87899c49d34bd24055e90
Shan <itsshankart44@gmail.com>
2020-03-22 20:35:15 +0530

    Updates

---
+++ precache-manifest.d9ac95cec2216762131395bf274c9ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446a7792b6aab5634c0c4a2c342f948c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d71832ef394f3bda5b20",
+    "url": "/weather-app/static/css/main.d5c15122.chunk.css"
+  },
+  {
+    "revision": "aa44e38617fb1f72ae35",
+    "url": "/weather-app/static/js/2.d70a1730.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather-app/static/js/2.d70a1730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71832ef394f3bda5b20",
+    "url": "/weather-app/static/js/main.cc856b46.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  }
+]);
